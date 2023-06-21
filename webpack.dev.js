aacf0e0ebdd2f04c6df3f738a651a99c57ee1036
Vincent Fretin <vincent.fretin@gmail.com>
2023-06-21 10:43:38 +0200

    Update all dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,16 @@
 const path = require("path");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    publicPath: "/dist/",
-    contentBase: path.resolve(__dirname, "examples")
+    server: {
+      type: 'https'
+    },
+    static: {
+      directory: path.resolve(__dirname, "examples")
+    }
   }
 });
