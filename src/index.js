ac13d8f81482b49d8cad51aad780010bfbbdb96e
arpu <a.putz@captic.io>
2024-04-30 13:22:40 +0200

    remove old firefox workaround

--- src/index.js
+++ src/index.js
@@ -948,12 +948,6 @@ class JanusAdapter {
         if (sender != null) {
           if (sender.replaceTrack) {
             sender.replaceTrack(t);
-
-            // Workaround https://bugzilla.mozilla.org/show_bug.cgi?id=1576771
-            if (t.kind === "video" && t.enabled && navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-              t.enabled = false;
-              setTimeout(() => t.enabled = true, 1000);
-            }
           } else {
             // Fallback for browsers that don't support replaceTrack. At this time of this writing
             // most browsers support it, and testing this code path seems to not work properly
